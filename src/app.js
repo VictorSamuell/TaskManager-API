6582a437a847f9c37b316e9594871592e70c51ff
VictorSamuell <victor.samuel1@aluno.ifsp.edu.br>
2025-02-17 23:21:24 -0300

    Project development

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
-print("Hello World");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Conectar ao MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+
+// Rotas
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+
+module.exports = app;
 
---
+++ src/controllers/UserController.js
@@ -0,0 +1,29 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).send({ user });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new Error('Credenciais inválidas');
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+module.exports = { register, login };
